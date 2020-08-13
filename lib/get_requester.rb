094b256921f164f956cf051479e5a8ddc807d903
Victor Avila <avilavictor1570@gmail.com>
2020-08-13 02:34:02 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -8,7 +8,7 @@ class GetRequester
   attr_reader :url
   
   def initialize(url)
-    @url = https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
+    @url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
   end
   
 end
